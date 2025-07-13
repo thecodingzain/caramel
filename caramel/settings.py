93826c14ab2c842a959663d518c4e5567a027fec
Reginald <reginaldateya@gmail.com>
2025-07-13 14:43:21 +0300

    Change from caramel/static to static

--- caramel/settings.py
+++ caramel/settings.py
@@ -84,7 +84,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'caramel/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # for production
 
 # Media files
